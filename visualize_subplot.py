8147fcd05cbcb79865a01e190c58c5be8c03af4b
pold87 <volker.strobel87@gmail.com>
2015-11-19 12:04:27 +0100

    Improved vis script; subplots are working

--- visualize_subplot.py
+++ visualize_subplot.py
@@ -34,18 +34,22 @@ def rotate_coordinates(xs, ys, theta):
     return xs_new, ys_new
 
 
+
 def show_graphs():
+    
 
     plt.ion()
     predictions = np.load("filtered.npy")
     path = "/home/pold87/Downloads/imgs_first_flight/"
 
     # First set up the figure, the axis, and the plot element we want to animate
-    plt.figure(1)
-    ax = plt.axes(xlim=(0, 4300), ylim=(-2343, 2343))
+    ax = plt.subplot2grid((2,2), (0, 0))
+    ax.set_xlim([0, 4300])
+    ax.set_ylim([-2343, 2343])
+
     line, = ax.plot([], [], lw=2)
 
-    plt.title('Predictions')
+    ax.set_title('Predictions')
 
     xs = predictions[:, 0]
     ys = predictions[:, 1]
@@ -55,25 +59,26 @@ def show_graphs():
     minidrone = read_png("img/minidrone.png")
     imagebox = OffsetImage(minidrone, zoom=1)
     background_map = plt.imread("../draug/img/bestmap.png")
-    plt.imshow(background_map, zorder=0, extent=[0, 4300, -2343, 2343])
+    ax.imshow(background_map, zorder=0, extent=[0, 4300, -2343, 2343])
 
-
-    plt.figure(3)
     optitrack = np.load("optitrack_coords.npy")
-    ax_opti = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
-    line_opti, = plt.plot([], [], lw=2)
+    ax_opti = plt.subplot2grid((2,2), (1, 0), colspan=2)
+    line_opti, = ax_opti.plot([], [], lw=2)
+    ax_opti.set_xlim([-10, 10])
+    ax_opti.set_ylim([-10, 10])
+
 
     xs_opti = optitrack[:, 0]
     ys_opti = optitrack[:, 1]
     ys_opti, xs_opti = rotate_coordinates(xs_opti, ys_opti, np.radians(37))
+
+    ax_inflight = plt.subplot2grid((2,2), (0, 1))
 
     for i in range(start_pic, len(xs)):
         img_path = path + str(i) + ".jpg"
 
         xy = (xs[i], ys[i])
 
-        plt.figure(1)
-        
         if i != start_pic:
             drone_artist.remove()
         ab = AnnotationBbox(imagebox, xy,
@@ -83,20 +88,17 @@ def show_graphs():
 
         line.set_xdata(xs[max(start_pic, i - 13):i])  # update the data
         line.set_ydata(ys[max(start_pic, i - 13):i])
-        drone_artist = ax.add_artist(ab)
 
-        plt.figure(3)
         line_opti.set_xdata(xs_opti[max(start_pic, i - 25):i])  # update the data
         line_opti.set_ydata(ys_opti[max(start_pic, i - 25):i])
-        print(xs_opti[max(start_pic, i - 13):i])
 
-        plt.figure(2)
         pic = mpimg.imread(img_path)
         if i == start_pic:
-            ax_img = plt.gca()
-            img_artist = ax_img.imshow(pic)
+            img_artist = ax_inflight.imshow(pic)
         else:
             img_artist.set_data(pic)
+
+        drone_artist = ax.add_artist(ab)
 
         plt.pause(.001)
 
