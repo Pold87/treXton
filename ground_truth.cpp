864362e0ab5719e44ceef1847a92c373eed99580
Volker Strobel <volker.strobel87@gmail.com>
2016-03-09 13:53:04 +0100

    reverted ground truth

--- ground_truth.cpp
+++ ground_truth.cpp
@@ -17,23 +17,28 @@ int main(int argc, char *argv[])
 
   if (argc == 1) {
     
-    cout << "***\nUsage: ./ground_truth <num_pics>\n***" << std::endl;
+    cout << "***\nUsage: ./ground_truth <num_pics> <basedir>\nOptionally, you can specify the output file as third argument***" << std::endl;
  
     return -1;
   }
 
-  // int num_pics = argv[1];
-  string basedir = "../image_recorder/playing_mat";
-  int num_pics = std::atoi(argv[1]);
+  int num_pics = std::atoi(argv[1]); /* The number of images that should be labeled*/
+  string basedir = argv[2]; /* The directory of the images */
 
-  // Read in background image
-  string background_map = "../draug/img/bestnewmat.png";
+  // Read in background image that is used for finding the homography  
+  string background_map = "/home/pold/Documents/Internship/draug/img/sparse_board.jpg";
   cv::Mat background_img = cv::imread(background_map);
+  int img_height = background_img.rows;
   int img_width = background_img.cols;
   
   /* Open file for saving estimates */
   FILE* fp;
-  fp = fopen("sift_targets.csv", "w");
+  const char *outfile = "sift_targets.csv";
+  if (argc >= 4)
+    {
+      outfile = argv[3];
+    }
+  fp = fopen(outfile, "w");
   fprintf(fp, "id,x,y,matches"); /* Print header */
 
   // Construct relocalizer with reference image path
@@ -49,7 +54,7 @@ int main(int argc, char *argv[])
 
     // Get estimation (x, y) in pixels from relocalizer
     std::vector<float> coords = relocalizer.calcLocation(img);
-    fprintf(fp, "\n%d,%f,%f,%d", i, coords[0], img_width - coords[1],(int) coords[2]);
+    fprintf(fp, "\n%d,%f,%f,%d", i, coords[0], img_height - coords[1],(int) coords[2]);
   }
 
   fclose(fp);
